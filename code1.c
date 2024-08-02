fd06f997ede4093e9e08dcd3ab410fb58caffb9a
100xEngineerai <vedantshinde501@gmail.com>
2024-08-03 02:18:50 +0530

    Second Code Added

--- code1.c
+++ code1.c
@@ -10,10 +10,10 @@ Task:
  */
 
 
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
 
 int main()
 {
