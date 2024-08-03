26aa9fecbf09c79d4ba8ec9ab3ba0553325706b2
100xEngineerai <vedantshinde501@gmail.com>
2024-08-03 21:11:12 +0530

    New code added

--- code1.c
+++ code1.c
@@ -19,12 +19,13 @@ int main()
 {
     int a, b;
     float c, d;
-
-    scanf("%d %d\n", &a, &b);
-    scanf("%f %f\n", &c, &d);
+    
+    printf("Enter numbers");
+    scanf("%d %d", &a, &b);
+    scanf("%f %f", &c, &d);
 
     printf( "%d %d\n", a+b, a-b);
-    printf( "%1.f %1.f\n", c+d, c-d);
+    printf( "%.1f %.1f\n", c+d, c-d);
 
     return 0;
 
